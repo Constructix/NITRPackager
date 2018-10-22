02e3871c6fb32dc47f7d70368b88d374c868a902
Richard Jones <r_jones@constructix.com.au>
2018-10-22 22:40:23 +1000

    Add project files.

---
+++ NITRPackager.Tests/SolutionExtractorTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NITRPackager.Extractors;
+using NITRPackager.Models;
+
+namespace NITRPackager.Tests
+{
+    public static class Constants
+    {
+        public const string  SolutionFileLocation = @"C:\Users\Richard\source\repos\NITRPackager\SampleSolution\DownloadFromNetTaskFactory\DownloadFromNetTaskFactory.sln";
+    }
+
+    [TestClass]
+    public class ProjectExtractor
+    {
+
+        [TestInitialize]
+        public void Initialise()
+        {
+
+        }
+
+        [TestMethod]
+        public void ProjectExtractorNoExceptionExpected()
+        {
+        }
+
+        [TestMethod]
+        public void ExtractCSharpFiles()
+        {
+            var fileName =@"C:\Users\Richard\source\repos\NITRPackager\SampleSolution\DownloadFromNetTaskFactory\DownloadFromNetTaskFactory\DownloadFromNetTaskFactory.csproj";
+
+            XDocument doc = XDocument.Load(fileName);
+
+            //get all csharp files.
+            var csharpFiles =  doc.Descendants().Where(x => x.Name.LocalName.Equals("Compile"));
+            foreach (XElement csharpFile in csharpFiles)
+            {
+                Console.WriteLine(csharpFile.FirstAttribute.Value);
+            }
+        }
+        
+    }
+
+    [TestClass]
+    public class SolutionExtractorTests
+    {
+        private SolutionExtractor extractor;
+        
+        [TestInitialize]
+        public void Initialise()
+        {
+            // baseline initialise extractor object.
+            extractor = new SolutionExtractor(Constants.SolutionFileLocation);
+        }
+
+        [TestMethod]
+        public void OpenSolutionThatIsPassedIntoExtractorConstructor()
+        {
+            Assert.IsNotNull(extractor);
+            var expectedSolutionLocation =Constants.SolutionFileLocation;
+            var actual = extractor.SolutionLocation;
+            Assert.IsTrue(expectedSolutionLocation.Equals(actual));
+        }
+
+        [TestMethod]
+        public void ExtractProjectsFromSolutionFileDoesNotExist()
+        {
+
+            //Arrange
+            const string solutionName = @"C:\Users\Richard\source\repos\NITRPackager\SampleSolution\DownloadFromNetTaskFactory1\DownloadFromNetTaskFactory.sln";
+            //Act
+            extractor = new SolutionExtractor(solutionName);
+            //ASSERT
+            var ex = Assert.ThrowsException<FileNotFoundException>(() => extractor.Extract());
+        }
+
+        [TestMethod]
+        public void  ExtractProjectsFromSolutionFileExistsNoExceptionExpected()
+        {
+            // Arrange
+            // use default solution file.
+            // Act
+            extractor = new SolutionExtractor(Constants.SolutionFileLocation);
+            //Assert
+            extractor.Extract();
+        }
+
+        [TestMethod]
+        public void ExtractProjectDetailsFromSolutionFile()
+        {
+            //Arrange
+            var expectedProjects = 1;
+            // Act
+            extractor = new SolutionExtractor(Constants.SolutionFileLocation);
+            extractor.Extract();
+            // Assert
+            Assert.IsTrue(expectedProjects == extractor.Projects.Count());
+
+            foreach (ProjectDetail currentProjectDetail in extractor.Projects)
+            {
+                Console.WriteLine($"{currentProjectDetail.Name} {currentProjectDetail.Location}");
+            }
+        }
+    }
+}
